019277907d3adacbdb8b440ba945517f57ce0112
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-05-25 15:02:58 +0200

    fixed string templating

--- app/src/main/java/com/example/androidprgeindopdracht/DetailActivity.java
+++ app/src/main/java/com/example/androidprgeindopdracht/DetailActivity.java
@@ -29,6 +29,13 @@ public class DetailActivity extends AppCompatActivity {
             person = getIntent().getSerializableExtra(Person.TAG, Person.class);
         }
 
-//        (TextView) findViewById(R.id.detail_activity_address)
+        ((TextView) findViewById(R.id.detail_activity_address)).setText(getString(R.string.address, person.houseNumber, person.street, person.city));
+        ((TextView) findViewById(R.id.detail_activity_country)).setText(person.country);
+        ((TextView) findViewById(R.id.detail_activity_dob)).setText(person.birthDate);
+        ((TextView) findViewById(R.id.detail_activity_gender)).setText(person.gender);
+        ((TextView) findViewById(R.id.detail_activity_email)).setText(person.email);
+        ((TextView) findViewById(R.id.detail_activity_name)).setText(getString(R.string.name, person.firstName, person.lastName));
+        ((TextView) findViewById(R.id.detail_activity_nationality)).setText(person.nationality);
+        ((TextView) findViewById(R.id.detail_activity_phone)).setText(person.phone);
     }
 }
